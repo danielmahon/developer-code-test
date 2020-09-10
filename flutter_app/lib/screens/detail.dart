d481ec3e2c4e53df1c437da55003d636e9c86d8c
Daniel Mahon <danielmahon@users.noreply.github.com>
2020-09-10 13:20:47 -0400

    fix: remove share for web demo
    Signed-off-by: Daniel Mahon <danielmahon@users.noreply.github.com>

--- flutter_app/lib/screens/detail.dart
+++ flutter_app/lib/screens/detail.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 import 'package:floating_pullup_card/floating_layout.dart';
 import 'package:floating_pullup_card/types.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -72,18 +73,19 @@ class DetailScreen extends HookWidget {
                   ],
                 ),
               ),
-              PopupMenuItem(
-                value: 'share',
-                child: Row(
-                  children: const [
-                    Padding(
-                      padding: EdgeInsets.only(right: 8.0),
-                      child: Icon(Icons.share, color: Colors.black),
-                    ),
-                    Text('Share'),
-                  ],
+              if (!kIsWeb)
+                PopupMenuItem(
+                  value: 'share',
+                  child: Row(
+                    children: const [
+                      Padding(
+                        padding: EdgeInsets.only(right: 8.0),
+                        child: Icon(Icons.share, color: Colors.black),
+                      ),
+                      Text('Share'),
+                    ],
+                  ),
                 ),
-              ),
             ],
             icon: const Icon(Icons.share),
           ),
@@ -168,7 +170,7 @@ class DetailScreen extends HookWidget {
                   backgroundDecoration: const BoxDecoration(
                     color: Colors.transparent,
                   ),
-                  initialScale: PhotoViewComputedScale.contained,
+                  // initialScale: PhotoViewComputedScale.contained,
                   imageProvider: AssetImage(src),
                 ),
               ),
